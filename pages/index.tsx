d93ce8fb610de48f044cbd9c511a41482da4048f
thefalked <giulainocrivelli@hotmail.com>
2021-05-06 01:46:39 -0400

    using high order function to verify guest on serverside

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useContext, useState } from "react";
 
 import { AuthContext } from "../context/AuthContext";
 import styles from "../styles/Home.module.css";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -40,19 +41,10 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies["nextauth.token"]) {
+export const getServerSideProps: GetServerSideProps = withSSRGuest(
+  async (ctx) => {
     return {
-      redirect: {
-        destination: "/dashboard",
-        permanent: false,
-      },
+      props: {},
     };
   }
-
-  return {
-    props: {},
-  };
-};
+);
