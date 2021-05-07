a92c8c16cf3e2d902be1c51516463b5246bcf4e7
thefalked <giulainocrivelli@hotmail.com>
2021-05-06 23:06:40 -0400

    adding broadcast for logout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -10,7 +10,7 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get("me").then((response) => console.log(response));
@@ -19,6 +19,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>dashboard {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+
       <Can permissions={["metrics.list"]}>
         <div>Métricas</div>
       </Can>
